46e5d207f7ce94896fa746c651cfcf4c8a0ce899
imh0 <igor.mayorow@gmail.com>
2016-03-05 21:30:07 +0200

    Init gh-pages

---
+++ tictactoe.js
@@ -0,0 +1,97 @@
+(function() {
+	var squares = [],
+	EMPTY = "\xA0",
+	score,
+	moves,
+	turn = "X",
+	oldOnLoad,
+	wins = [7, 56, 448, 73, 146, 292, 273, 84],
+
+	// Clears the score and move count, erases the board, and makes it
+	// X's turn
+	startNewGame = function () {
+		var i;
+		turn = "X";
+		score = {"X" : 0, "O" : 0};
+		moves = 0;
+		for (i = 0; i < squares.length; i += 1) {
+			squares[i].firstChild.nodeValue = EMPTY;
+		}
+	},
+
+	// Returns whether the given score is winning score.
+	win = function (score) {
+		var i;
+		for (i = 0; i < wins.length; i += 1) {
+			if ((wins[i] & score) === wins[i]) {
+				return true;
+			}
+		}
+		return false;
+	},
+
+	// Sets the clicked-on square to the current player's mark,
+	// then checks for a win or cats game. Also changes the
+	// current player.
+	set = function() {
+		if (this.firstChild.nodeValue !== EMPTY) {
+			return;
+		}
+		this.firstChild.nodeValue = turn;
+		moves += 1;
+		score[turn] += this.indicator;
+		if (win(score[turn])) {
+			alert(turn + " wins!");
+			startNewGame();
+		}
+		else if (moves === 9) {
+			alert("Cat\u2019s game!");
+			startNewGame();
+		}
+		else {
+			turn = turn === "X" ? "O" : "X";
+		}
+	},
+	
+	// Creates and attaches the DOM elements for the board as an 
+	// HTML table, assigns the indicators for each cell, and starts
+	// a new game.
+	play = function () {
+		var board = document.createElement("table"),
+		indicator = 1,
+		i, j,
+		row, cell,
+		parent;
+		board.border = 1;
+		for (i = 0; i < 3; i += 1) {
+			row = document.createElement("tr");
+			board.appendChild(row);
+			for (j = 0; j < 3; j += 1) {
+				cell = document.createElement("td");
+				cell.width = cell.height = 50;
+				cell.align = cell.valign = 'center';
+				cell.indicator = indicator;
+				cell.onclick = set;
+				cell.appendChild(document.createTextNode(""));
+				row.appendChild(cell);
+				squares.push(cell);
+				indicator += indicator;
+			}
+		}
+		// Attach under tictactoe if present, otherwise to body.
+		parent = document.getElementById("tictactoe") || document.body;
+		parent.appendChild(board);
+		startNewGame();
+	};
+
+	// Add the play function to the (virtual) list of onload events.
+	if (typeof window.onload === "function") {
+		oldOnLoad = window.onload;
+		window.onload = function() {
+			oldOnLoad();
+			play();
+		};
+	} else {
+		window.onload = play;
+	}
+}());
